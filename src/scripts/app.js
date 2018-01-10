d32ca927dcb29192003246889627ec1c82dac4bb
Liza Chu <lozarti@mail.ru>
2018-01-10 15:45:39 +0300

    full menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -114,3 +114,47 @@ function initMap() {
 };
 
 initMap();
+
+
+var parallax = (function () {
+    var bg = document.querySelector('.hero');
+    var user = document.querySelector('.hero__user');
+
+    return {
+        move: function (block, windowScroll, strafeAmount) {
+            var strafe = windowScroll / -strafeAmount + '%';
+            var transformString = 'translate3d(0,' + strafe + ', 0)';
+            var style = block.style;
+
+            style.top = block.style;
+
+            style.transform = transformString;
+            style.webkitTransform = transformString;
+        },
+
+        init: function (wScroll) {
+            this.move(bg, wScroll, 45);
+            this.move(user, wScroll, 3);
+        }
+    }
+}());
+
+window.onscroll = function () {
+    var wScroll = window.pageYOffset;
+
+    parallax.init(wScroll);
+}
+
+
+
+var hero = document.getElementById('about__wrapper')
+var hamburger = document.getElementById('hamburger')
+var cross = document.getElementById('cross')
+
+hamburger.addEventListener('click', function() {
+    hero.classList.add('full-menu')
+});
+
+cross.addEventListener('click', function() {
+  hero.classList.remove('full-menu')
+});
